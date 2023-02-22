8b46b7a1e8c169b0cabe5532ec98670f4f8b8637
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-02-22 15:08:27 -0700

    Add puts statements

--- variables.rb
+++ variables.rb
@@ -10,3 +10,49 @@ dr_pepper = 3.25
 deposit_paid = true
 early_pay = false
 late_fees = false
+
+puts name
+puts email
+puts team_name
+puts max_team_size
+puts min_team_size
+puts max_teams
+puts coke
+puts sprite
+puts dr_pepper
+puts deposit_paid
+puts early_pay
+puts late_fees
+
+name = "Steve Barry"
+email = "sb@email.com"
+team_name = "cats"
+
+puts name
+puts email
+puts team_name
+
+max_team_size = 8
+min_team_size = 1
+max_teams = 6
+
+puts max_team_size
+puts min_team_size
+puts max_teams
+
+coke = 2.50
+sprite = 2.75
+dr_pepper = 2.22
+
+deposit_paid = false
+early_pay = true
+late_fees = true
+
+puts coke
+puts sprite
+puts dr_pepper
+
+puts deposit_paid
+puts early_pay
+puts late_fees 
+
